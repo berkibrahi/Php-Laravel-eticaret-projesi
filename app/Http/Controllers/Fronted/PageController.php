037b86789ee99d7f0c9afa93cc1e58f1c147c86e
berkibrahi <ibrahimberk076@gmail.com>
2024-06-30 18:15:33 +0300

    Ürün Filtreleme

--- app/Http/Controllers/Fronted/PageController.php
+++ app/Http/Controllers/Fronted/PageController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\isEmpty;
+
 class PageController extends Controller
 {
 
@@ -18,8 +20,28 @@ class PageController extends Controller
     public function about(){
         return view("fronted.pages.about");
     }
-    public function products(){
-        $products=Product::where("status","0")->paginate(1);
+    public function products(Request $request){
+        $size=$request->size ?? null;
+        $color=$request->color ?? null;
+
+        $startPrice=$request->start_price ?? null;
+
+        $endPrice=$request->end_price ?? null;
+        $products = Product::where("status", "0")->where(function($q) use($size, $color,$startPrice,$endPrice) {
+            if (!empty($size)) {
+                $q->where("size", $size);
+            }
+            if (!empty($color)) {
+                $q->where("color", $color);
+            }
+            if (!empty($startPrice) && $endPrice) {
+                $q->whereBetween("price", [$startPrice,$endPrice]);
+            }
+            return $q;
+        });
+
+        // Sayfalama için paginate metodunu kullanın
+        $products = $products->paginate(1);
         return view("fronted.pages.products",compact("products"));
 
     }
